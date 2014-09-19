4aecc1792fa7fb53e07687fe73100d084acd1b5c
Kevin Haire <khaire@shopcity.com>
2014-09-19 09:24:21 -0400

    removed redirect if referrer has the word shop in the domain name for linking from shopcity mobile site

--- inc/_redirect.php
+++ inc/_redirect.php
@@ -1,4 +1,11 @@
 <?php
+	$referer = $_SERVER["HTTP_REFERER"];
+	$url = $_SERVER["HTTP_HOST"];
+
+	if (strpos($referer,'shop') !== false || strpos($referer, $url) !== false) {
+		$redirect = "false";
+	}
+
 	if ($redirect == "true") {
 		$iphone  = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
 		$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
